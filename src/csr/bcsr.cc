db311a212db74a530674514da8e4cef6dc957fb6
Valentin Servières <vservieres@laas.fr>
2025-06-06 15:17:45 +0200

    feat: initialising bcsr

--- src/csr/bcsr.cc
+++ src/csr/bcsr.cc
@@ -1,10 +1,39 @@
 #include "bcsr.hh"
 
+#include <sys/types.h>
+#include <vector>
+#include <iostream>
 
-CSR::CSR(/* args */)
+
+
+void insertDn2BCSR(std::vector<u_int32_t> &i_ptr, std::vector<u_int32_t> &indces, u_int32_t width, u_int32_t height, u_int8_t **values)
 {
+    i_ptr[0] = 0;
+
+    for (u_int32_t row = 0; row < height; ++row)
+    {
+        for (u_int32_t col = 0; col < width; ++col)
+        {
+            if (values[row][col])
+            {
+                indces.push_back(col);
+            }
+        }
+        i_ptr[row + 1] = indces.size();
+    }
 }
 
-CSR::~CSR()
+BCSR::BCSR(u_int32_t width, u_int32_t height) : _width(width), _height(height)
 {
+    _index_pointers = std::vector<u_int32_t>(height + 1);
+    _indices = std::vector<u_int32_t>();
 }
+
+BCSR::BCSR(u_int32_t width, u_int32_t height, u_int8_t **values) : _width(width), _height(height)
+{
+    _index_pointers = std::vector<u_int32_t>(height + 1);
+    _indices = std::vector<u_int32_t>();
+}
+// CSR::~CSR()
+// {
+// }
--- src/csr/bcsr.hh
+++ src/csr/bcsr.hh
@@ -2,13 +2,21 @@
 #ifndef BCSR_HH
 #define BCSR_HH
 
-class CSR
+#include <sys/types.h>
+#include <vector>
+
+class BCSR
 {
 private:
-    
+    u_int32_t _width, _height;
+    std::vector<u_int32_t> _index_pointers;
+    std::vector<u_int32_t> _indices;
+
 public:
-    CSR(/* args */);
-    ~CSR();
+    BCSR(u_int32_t width, u_int32_t height);
+    BCSR(u_int32_t width, u_int32_t height, u_int8_t **values);
+    u_int8_t toDense();
+    // ~BCSR();
 };
 
 #endif // BCSR_HH
