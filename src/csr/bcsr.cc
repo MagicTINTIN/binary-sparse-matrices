c1da93dc4653ddbfe49c58201163d21c597c3c81
MagicTINTIN <magictintin@proton.me>
2025-06-11 11:23:31 +0200

    wip: implementing a more optimized or

--- src/csr/bcsr.cc
+++ src/csr/bcsr.cc
@@ -41,13 +41,47 @@ void BCSR::operationOr(const BCSR &b)
     }
 
     // FIXME: (opti?) _index_pointers.size()  -> _height + 1
-    for (size_t r = 1; r < _index_pointers.size(); r)
-    {
-        for (size_t c = b._index_pointers[r - 1]; c < b._index_pointers[r]; c++)
-        {
-            set(r - 1, b._indices[c]);
-        }
+    // for (size_t r = 1; r < _index_pointers.size(); r)
+    // {
+    //     for (size_t c = b._index_pointers[r - 1]; c < b._index_pointers[r]; c++)
+    //     {
+    //         set(r - 1, b._indices[c]);
+    //     }
+    // }
 
+    // for each row after an insertion, propagate the information with the carry
+    u_int32_t carry = 0;
+    for (size_t r = 1; r < _index_pointers.size(); r++)
+    {
+        // if the line contains a non zero value.
+        if (b._index_pointers[r] - b._index_pointers[r - 1] > 0)
+        {
+            u_int8_t addedInRow = 0;
+            u_int32_t b_index_pointer = b._index_pointers[r - 1];
+            // for each column check if there is an insertion to do before it
+            for (u_int32_t col_index = _index_pointers[r - 1]; col_index < _index_pointers[r] + carry; col_index++)
+            {
+                // add every non zero indice (column) before this column
+                for (; b._indices[b_index_pointer] <= _indices[col_index] && b_index_pointer < b._index_pointers[r]; b_index_pointer++)
+                {
+                    // if the value was already 1, then there is no change for this column
+                    if (b._indices[b_index_pointer] < _indices[col_index]) {
+                        _indices.insert(_indices.begin() + col_index, b._indices[b_index_pointer]);
+                        addedInRow++;
+                        carry++;
+                        col_index++;
+                    }
+                }
+            }
+            // for every other non-zero in the line that has not been added
+            for (; b_index_pointer < b._index_pointers[r]; b_index_pointer++)
+            {
+                _indices.insert(_indices.begin() + _index_pointers[r] + addedInRow, b._indices[b_index_pointer]);
+                carry++;
+                addedInRow++;
+            }
+        }
+        _index_pointers[r] += carry;
     }
 }
 
@@ -63,21 +97,21 @@ BCSR BCSR::operator|(const BCSR &b) const
     BCSR result(*this);
 
     // FIXME: (opti?) _index_pointers.size()  -> _height + 1
-    for (size_t r = 1; r < _index_pointers.size(); r)
-    {
-        for (size_t c = b._index_pointers[r - 1]; c < b._index_pointers[r]; c++)
-        {
-            result.set(r - 1, b._indices[c]);
-        }
-    }
+    // for (size_t r = 1; r < _index_pointers.size(); r)
+    // {
+    //     for (size_t c = b._index_pointers[r - 1]; c < b._index_pointers[r]; c++)
+    //     {
+    //         result.set(r - 1, b._indices[c]);
+    //     }
+    // }
 
     return result;
 }
 
-
-void BCSR::set(const u_int32_t row, const u_int32_t col, const u_int8_t value) {
+void BCSR::set(const u_int32_t row, const u_int32_t col, const u_int8_t value)
+{
     if (value)
-        set(row,col);
+        set(row, col);
     else
         reset(row, col);
 }
@@ -146,99 +180,6 @@ void BCSR::reset(const u_int32_t row, const u_int32_t col)
             }
         }
         _index_pointers[r] -= 1;
-    }
-}
-
-std::vector<u_int8_t> BCSR::toDenseVector() const
-{
-    std::vector<u_int8_t> mat(_width * _height);
-
-    for (int i = 0; i < _height; ++i)
-    {
-        for (int idx = _index_pointers[i]; idx < _index_pointers[i + 1]; ++idx)
-        {
-            mat[i * _width + _indices[idx]] = 1;
-        }
-    }
-
-    return mat;
-}
-
-std::string BCSR::toString() const
-{
-    if (_width == 0 || _height == 0)
-        return std::string("<0;0>");
-
-    std::string ret("<");
-    ret += std::to_string(_height) + ";" + std::to_string(_width) + ">\nIndex Pointers (Rows): [" + std::to_string(_index_pointers[0]);
-    for (u_int32_t i = 1; i < _height + 1; i++)
-        ret += "|" + std::to_string(_index_pointers[i]);
-    ret += "]\nIndices (Columns): [";
-    if (_indices.size() > 0)
-        ret += std::to_string(_indices[0]);
-    for (u_int32_t i = 1; i < _indices.size(); i++)
-        ret += "|" + std::to_string(_indices[i]);
-    ret += "]";
-    return ret;
-}
-
-std::string BCSR::toDnString() const
-{
-    if (_height == 0)
-        return std::string("[]");
-    if (_width == 0)
-    {
-        std::string ret("[[]");
-        for (size_t i = 1; i < _height; i++)
-        {
-            ret += ",[]";
-        }
-        return ret + "]";
-    }
-
-    std::string ret("[");
-    std::vector<u_int8_t> m = toDenseVector();
-    for (size_t line = 0; line < _height; line++)
-    {
-        if (line == 0)
-        {
-            ret += "[";
-        }
-        else
-            ret += ",\n [";
-        for (size_t col = 0; col < _width; col++)
-        {
-            if (col == 0)
-            {
-                ret += std::to_string(m[line * _width + col]);
-            }
-            else
-            {
-                ret += "," + std::to_string(m[line * _width + col]);
-            }
-        }
-        ret += "]";
-    }
-    ret += "]";
-    return ret;
-}
-
-void BCSR::insertDn2BCSR(u_int8_t values[])
-{
-    _index_pointers[0] = 0;
-
-    for (u_int32_t row = 0; row < _height; ++row)
-    {
-        for (u_int32_t col = 0; col < _width; ++col)
-        {
-            if (values[row * _width + col])
-            {
-                _indices.push_back(col);
-                // printf("adding {%d;%d}\n", row, col);
-            }
-        }
-        // printf("row{%d}=%d\n", row + 1, _indices.size());
-        _index_pointers[row + 1] = _indices.size();
     }
 }
 
--- src/main.cc
+++ src/main.cc
@@ -20,8 +20,17 @@ int main(int argc, char const *argv[])
         0, 0, 1, 1, 1,
         0, 0, 0, 0, 0,
         0, 0, 0, 1, 0};
+    u_int8_t bmat2[7 * 5] = {
+        1, 0, 0, 0, 1,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        1, 0, 0, 0, 1};
 
     BCSR bcsr_mat(7, 5, bmat);
+    BCSR bcsr_mat2(7, 5, bmat2);
 
     std::cout << bcsr_mat << "\n"
               << bcsr_mat.toDnString() << std::endl;
@@ -35,7 +44,11 @@ int main(int argc, char const *argv[])
     bcsr_mat.set(6, 0);
     bcsr_mat.reset(0, 2);
     bcsr_mat.reset(0, 1);
+    std::cout << bcsr_mat << "\n";
+    std::cout << bcsr_mat.toDnString() << std::endl;
 
+    bcsr_mat |= bcsr_mat2;
+    std::cout << bcsr_mat << "\n";
     std::cout << bcsr_mat.toDnString() << std::endl;
     return 0;
 }
