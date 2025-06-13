aedd685609a7175cc380ba896b65c60b561dc06a
MagicTINTIN <magictintin@proton.me>
2025-06-13 16:45:35 +0200

    feat: test zone & chrono

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,69 @@
 #include <stdio.h>
 #include <iostream>
+#include <chrono>
 #include "csr/bcsr.hh"
+
+// using namespace std;
+// using namespace std::chrono;
+
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::microseconds;
+using std::chrono::time_point;
+
+#define NORMAL "\e[0m"
+#define BOLD "\e[1m"
+#define CYAN_NORMAL_COLOR "\e[36m"
+#define CYAN_DESAT_COLOR "\e[96m"
+
+#include <thread>
+
+void long_operation()
+{
+    /* Simulating a long, heavy operation. */
+
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(150ms);
+}
+
+// time_point start,stop;
+auto start = high_resolution_clock::now();
+auto stop = high_resolution_clock::now();
+void c_go()
+{
+    start = high_resolution_clock::now();
+}
+
+void c_stop(std::string msg)
+{
+    stop = high_resolution_clock::now();
+
+    /* Getting number of milliseconds as an integer. */
+    // auto ms_int = duration_cast<milliseconds>(stop - start);
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> ms_double = stop - start;
+
+    // std::cout << ms_int.count() << "ms\n";
+    std::cout << ms_double.count() << "ms\n";
+
+    std::cout << BOLD << CYAN_NORMAL_COLOR << "⏰ CHORONO[" << CYAN_DESAT_COLOR << msg << CYAN_NORMAL_COLOR << "]: "
+              << ms_double.count() << " ms" << NORMAL << std::endl;
+}
+
+void c_stop()
+{
+    c_stop("");
+}
 
 int main(int argc, char const *argv[])
 {
     std::cout << "Starting...\n";
+    // c_go();
+    // long_operation();
+    // c_stop();
+
     u_int8_t mat[7 * 5] = {
         8, 0, 2, 0, 0,
         0, 0, 5, 0, 0,
@@ -97,14 +156,19 @@ int main(int argc, char const *argv[])
     // std::cout << bcsr4.transpose().transpose() << "\n";
     // std::cout << bcsr4.transpose().toDnString() << std::endl;
 
-
-    int A4_p[6] = {0,2,4,7,10,10};
-    int A4_j[10] = {2,7,0,7,2,5,6,1,3,5};
+    int A4_p[6] = {0, 2, 4, 7, 10, 10};
+    int A4_j[10] = {2, 7, 0, 7, 2, 5, 6, 1, 3, 5};
     int R4_p[9] = {0};
     int R4_j[10] = {0};
-    scipy_tocsc(5,8,A4_p,A4_j,R4_p,R4_j);
+    c_go();
+    scipy_tocsc(5, 8, A4_p, A4_j, R4_p, R4_j);
+    c_stop("Scipy");
 
-    std::cout << "Scipy:\n" << scipy_tostr(8, 10, R4_p, R4_j) << "\n";
+    c_go();
+    bcsr4.transpose();
+    c_stop("Moi");
+    std::cout << "Scipy:\n"
+              << scipy_tostr(8, 10, R4_p, R4_j) << "\n";
 
     printf("###################################################\n");
 
@@ -203,8 +267,97 @@ int main(int argc, char const *argv[])
     int R8_p[10] = {0};
     int R8_j[10] = {0};
 
+    c_go();
     scipy_csr_matmat_binary(4, 4, A8_p, A8_j, B8_p, B8_j, R8_p, R8_j);
-    std::cout << "Scipy:\n" << scipy_tostr(4, 8, R8_p, R8_j) << "\n";
+    c_stop("Scipy");
 
+    c_go();
+    bcsr8 *bcsr8bis;
+    c_stop("Moi");
+    std::cout << "Scipy:\n"
+              << scipy_tostr(4, 8, R8_p, R8_j) << "\n";
+
+    printf("###################################################\n");
+    printf("################### BENCH TESTS ###################\n");
+    printf("###################################################\n");
+    printf("#################### TRANSPOSE ####################\n");
+
+    u_int8_t _T1[] = {
+        0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+        0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    BCSR T1(5, 8, _T1);
+    BCSR T1t(T1.transpose());
+    BCSR T1tt(T1t.transpose());
+    std::cout << T1tt << "\n"
+              << T1.toCondensedString(',') << "\n"
+              << T1t << "\n";
+
+    int T1A_p[6] = {0, 2, 4, 7, 10, 10};
+    int T1A_j[10] = {2, 7, 0, 7, 2, 5, 6, 1, 3, 5};
+    int T1R_p[9] = {0};
+    int T1R_j[10] = {0};
+
+    c_go();
+    scipy_tocsc(5, 8, T1A_p, T1A_j, T1R_p, T1R_j);
+    c_stop("Scipy");
+
+    c_go();
+    T1.transpose();
+    c_stop("Moi");
+    std::cout << "Scipy:\n"
+              << scipy_tostr(8, 10, T1R_p, T1R_j) << "\n";
+
+    printf("################## MULTIPLICATION #################\n");
+
+    u_int8_t _T2A[4 * 5] = {
+        1, 0, 1, 1, 1,
+        0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0,
+        1, 1, 0, 0, 0};
+
+    u_int8_t _T2B[5 * 4] = {
+        0, 1, 1, 0,
+        1, 0, 0, 0,
+        1, 0, 0, 0,
+        0, 0, 1, 0,
+        1, 0, 0, 1};
+
+    BCSR T2A(4, 5, _T2A), T2B(5, 4, _T2B);
+
+    std::cout << T2A.toCondensedString(',') << "\n"
+              << T2B.toCondensedString(',') << "\n";
+    BCSR T2R(T2A * T2B);
+    std::cout << T2R.toCondensedString() << "\n";
+    // std::cout << T2Ares.toDnString() << "\n";
+
+    int T2A_p[5] = {0, 4, 5, 5, 7};
+    int T2A_j[7] = {0, 2, 3, 4, 3, 0, 1};
+    int T2B_p[6] = {0, 2, 3, 4, 5, 7};
+    int T2B_j[7] = {1, 2, 0, 0, 2, 0, 3};
+    int T2R_p[10] = {0};
+    int T2R_j[10] = {0};
+
+    c_go();
+    scipy_csr_matmat_binary(4, 4, T2A_p, T2A_j, T2B_p, T2B_j, T2R_p, T2R_j);
+    c_stop("Scipy");
+
+    c_go();
+    T2A *T2B;
+    c_stop("Moi");
+    std::cout << "Scipy:\n"
+              << scipy_tostr(4, 8, T2R_p, T2R_j) << "\n";
     return 0;
 }
