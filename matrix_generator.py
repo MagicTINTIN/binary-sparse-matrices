2f392ec6aaae957943fdbd08611d31c0d4d8c4f0
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-06-14 19:22:32 +0200

    test: testing with big matrices

--- matrix_generator.py
+++ matrix_generator.py
@@ -5,17 +5,21 @@ def main():
     height = int(input("Matrix height: "))
     width = int(input("Matrix width: "))
     p = float(input("1-value probability : "))
+    oneliner = input("one-liner (-/no) : ")
 
     data = [
         1 if random.random() < p else 0
         for _ in range(height * width)
     ]
 
-    print(f"u_int8_t mat[{height}*{width}] = {{")
     
-    for i in range(height):
-        row = data[i*width:(i+1)*width]
-        print("    " + ", ".join(str(v) for v in row) + ("," if i < height - 1 else ""))
+    if oneliner == "no":  
+        print(f"u_int8_t mat[{height}*{width}] = {{")
+        for i in range(height):
+            row = data[i*width:(i+1)*width]
+            print("    " + ", ".join(str(v) for v in row) + ("," if i < height - 1 else ""))
+    else:
+        print(f"u_int8_t mat[{height}*{width}] = {{" + ", ".join(str(v) for v in data), end="")
     
     print("};")
 
